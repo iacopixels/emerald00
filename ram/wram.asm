460e50099ac572366e72c466f298c5746c9810b3
iacopixels <iacopixels@gmail.com~>
2026-04-14 22:35:01 -0300

    expand anim tls

--- ram/wram.asm
+++ ram/wram.asm
@@ -3632,8 +3632,7 @@ wScratchTilemap:: ds TILEMAP_AREA
 wScratchAttrmap:: ds TILEMAP_AREA
 
 NEXTU
-wDecompressScratch:: ds $80 tiles
-wDecompressEnemyFrontpic:: ds $80 tiles
+wDecompressScratch:: ds $100 tiles
 
 NEXTU
 ; unidentified uses
