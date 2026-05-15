73ecb905c29f39ab7561ace9482bf123dd35cb3c
iacopixels <iacopixels@gmail.com~>
2026-05-14 21:11:04 -0300

    trckInsd1

--- maps/UndergroundPath.asm
+++ maps/UndergroundPath.asm
@@ -13,13 +13,13 @@ UndergroundPath_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  3,  2, ROUTE_5_UNDERGROUND_PATH_ENTRANCE, 3
+	warp_event  3, 22, ROUTE_5_UNDERGROUND_PATH_ENTRANCE, 3
 	warp_event  3, 24, ROUTE_6_UNDERGROUND_PATH_ENTRANCE, 3
 
 	def_coord_events
 
 	def_bg_events
-	bg_event  3,  9, BGEVENT_ITEM, UndergroundPathHiddenFullRestore
-	bg_event  1, 19, BGEVENT_ITEM, UndergroundPathHiddenXSpecial
+	bg_event  1, 22, BGEVENT_ITEM, UndergroundPathHiddenFullRestore
+	bg_event  1, 24, BGEVENT_ITEM, UndergroundPathHiddenXSpecial
 
 	def_object_events
