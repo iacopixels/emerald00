6318dc0bd5313dfa9d0c10b2466ef5c5ee783dd0
iacopixels <iacopixels@gmail.com~>
2026-06-04 23:08:53 -0300

    rival msc

---
+++ audio/music/rslookyoungster.asm
@@ -0,0 +1,419 @@
+Music_RSLookYoungster:
+	musicheader 4, 1, Music_RSLookYoungster_Ch1
+	musicheader 1, 2, Music_RSLookYoungster_Ch2
+	musicheader 1, 3, Music_RSLookYoungster_Ch3
+	musicheader 1, 4, Music_RSLookYoungster_Ch4
+
+
+Music_RSLookYoungster_Ch1:
+	vibrato $12, $15
+	tempo 136
+	tone $0001
+	notetype $6, $92
+	dutycycle $0
+	octave 4
+	note D#, 2
+	note C_, 2
+	note D#, 2
+	note __, 6
+	note D#, 2
+	note C_, 2
+	note D#, 2
+	note __, 14
+Music_RSLookYoungster_Ch1_loop_main:
+	dutycycle $2
+	intensity $82
+	octave 4
+	note G_, 6
+	octave 5
+	note C_, 6
+	intensity $86
+	octave 4
+	note G_, 6
+	intensity $72
+	dutycycle $0
+	note C_, 4
+	note C_, 2
+	octave 3
+	note D#, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note A#, 2
+	octave 4
+	note C_, 2
+	dutycycle $2
+	intensity $82
+	note D#, 6
+	note F_, 6
+	intensity $86
+	note G_, 6
+	intensity $72
+	dutycycle $0
+	octave 3
+	note C_, 4
+	note C_, 2
+	intensity $70
+	octave 2
+	note A#, 4
+	octave 3
+	note C_, 4
+	note C#, 16
+	intensity $77
+	note C#, 14
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	intensity $74
+	note C#, 6
+	note D#, 6
+	intensity $70
+	note C#, 12
+	intensity $77
+	note C#, 8
+	
+	intensity $92
+	note F_, 6
+	note D#, 6
+	note D_, 4
+	note C_, 6
+	octave 2
+	note A#, 6
+	note A_, 4
+	dutycycle $2
+	octave 4
+	note C_, 6
+	note A_, 6
+	intensity $96
+	note F_, 6
+	dutycycle $0
+	intensity $92
+	octave 3
+	note D#, 4
+	note D#, 2
+	note C_, 4
+	note D#, 4
+	dutycycle $2
+	intensity $92
+	note F_, 4
+	octave 4
+	note F_, 2
+	note F_, 4
+	note D_, 2
+	note F_, 8
+	note D_, 4
+	note F_, 8
+	dutycycle $0
+	intensity $72
+	note F_, 6
+	note F_, 4
+	note F_, 2
+	octave 5
+	note D_, 4
+	octave 4
+	note A#, 16
+	loopchannel 0, Music_RSLookYoungster_Ch1_loop_main
+	
+Music_RSLookYoungster_Ch2:
+	vibrato $12, $15
+	tone $0001
+	notetype $6, $a0
+	dutycycle $3
+	octave 4
+	note G_, 2
+	note D#, 2
+	note G_, 2
+	note __, 6
+	note G_, 2
+	note D#, 2
+	note G_, 2
+	note __, 6
+	intensity $a7
+	note D#, 2
+Music_RSLookYoungster_Ch2_loop_main:
+	note G_, 2
+	note G#, 2
+	note A#, 2
+	intensity $a3
+	octave 5
+	note C_, 6
+	note D#, 6
+	intensity $a6
+	octave 4
+	note A#, 8
+	intensity $a3
+	note A#, 2
+	note G#, 2
+	note G_, 2
+	note G#, 2
+	note A#, 2
+	note D#, 2
+	note G_, 6
+	note G#, 6
+	intensity $a6
+	note A#, 7
+	note F#, 1
+	note G_, 4
+	note F_, 4
+	note D#, 4
+	intensity $a3
+	note F#, 6
+	intensity $a6
+	octave 5
+	note C#, 6
+	octave 4
+	note A#, 16
+	note __, 4
+	intensity $a3
+	note F#, 6
+	intensity $a6
+	octave 5
+	note C#, 6
+	octave 4
+	note A#, 8
+	note D#, 2
+	note C#, 2
+	octave 3
+	note A#, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note A#, 2
+	intensity $a3
+	note A_, 6
+	note G_, 6
+	note F_, 3
+	note E_, 1
+	note F_, 6
+	note D_, 6
+	note C_, 3
+	note E_, 1
+	note F_, 6
+	intensity $a6
+	octave 5
+	note C_, 4
+	octave 4
+	note F_, 1
+	note G#, 1
+	note A_, 8
+	intensity $a3
+	note D#, 2
+	note D_, 2
+	note C_, 2
+	note D_, 2
+	note F_, 2
+	note A_, 2
+	octave 3
+	note A#, 4
+	octave 4
+	note A#, 2
+	note A#, 4
+	note G#, 2
+	note A#, 8
+	note G#, 4
+	note A#, 8
+	note A#, 6
+	note A#, 4
+	note A#, 2
+	octave 5
+	note F_, 4
+	note D_, 8
+	intensity $a7
+	octave 4
+	note F_, 2
+	loopchannel 0, Music_RSLookYoungster_Ch2_loop_main
+	
+Music_RSLookYoungster_Ch3:
+	tone $0001
+	notetype $6, $13
+	octave 3
+	note A#, 2
+	note G_, 2
+	note A#, 2
+	note __, 6
+	note A#, 2
+	note G_, 2
+	note A#, 2
+	note __, 2
+	octave 2
+	note D#, 1
+	note __, 1
+	note D#, 4
+	note __, 2
+	note D#, 1
+	note __, 1
+	note D#, 1
+	note __, 1
+Music_RSLookYoungster_Ch3_loop_main:
+	octave 2
+	note D#, 3
+	note __, 1
+	note D#, 3
+	note __, 1
+	note D#, 3
+	note __, 1
+	note D#, 2
+	octave 3
+	note D#, 2
+	octave 2
+	note D#, 4
+	note A#, 4
+	note D#, 4
+	note G_, 2
+	note D#, 1
+	note __, 1
+	note D#, 3
+	note __, 1
+	note D#, 3
+	note __, 1
+	note D#, 2
+	note G_, 2
+	octave 3
+	note C_, 2
+	note D#, 4
+	octave 2
+	note D#, 3
+	note __, 1
+	note D#, 2
+	note A#, 4
+	note G#, 4
+	note F#, 3
+	note __, 1
+	note F#, 3
+	note __, 1
+	note F#, 3
+	note __, 1
+	note F#, 2
+	octave 3
+	note F#, 2
+	octave 2
+	note F#, 4
+	octave 3
+	note C#, 4
+	octave 2
+	note F#, 4
+	note A#, 2
+	note F#, 1
+	note __, 1
+	note F#, 3
+	note __, 1
+	note F#, 3
+	note __, 1
+	note F#, 2
+	note A#, 2
+	octave 3
+	note D#, 2
+	note F#, 4
+	octave 2
+	note F#, 3
+	note __, 1
+	note F#, 2
+	note G#, 4
+	note F#, 4
+	note F_, 3
+	note __, 1
+	note F_, 3
+	note __, 1
+	note F_, 3
+	note __, 1
+	note F_, 2
+	octave 3
+	note F_, 2
+	octave 2
+	note F_, 4
+	octave 3
+	note C_, 4
+	octave 2
+	note F_, 4
+	note A_, 2
+	note F_, 1
+	note __, 1
+	note F_, 5
+	note __, 1
+	note F_, 5
+	note __, 1
+	note F_, 5
+	note __, 1
+	note F_, 3
+	note __, 1
+	note F_, 2
+	note D#, 4
+	note C_, 4
+	octave 1
+	note A#, 3
+	note __, 1
+	note A#, 1
+	note __, 1
+	note A#, 6
+	octave 2
+	note A#, 2
+	octave 1
+	note A#, 1
+	note __, 1
+	note A#, 4
+	octave 2
+	note G#, 2
+	note A#, 2
+	octave 1
+	note A#, 4
+	octave 2
+	note G#, 2
+	note A#, 2
+	octave 1
+	note A#, 4
+	octave 2
+	note A#, 2
+	octave 1
+	note A#, 3
+	note __, 1
+	note A#, 4
+	octave 2
+	note A#, 2
+	octave 1
+	note A#, 16
+	loopchannel 0, Music_RSLookYoungster_Ch3_loop_main
+	
+Music_RSLookYoungster_Ch4:
+	togglenoise $3
+	notetype $c
+	note B_, 12
+Music_RSLookYoungster_Ch4_loop_main:
+	note C_, 1
+	note C_, 1
+	note C_, 1
+	note C_, 1
+Music_RSLookYoungster_Ch4_loop_1:
+	note D#, 2
+	note F#, 2
+	note C_, 2
+	note G#, 1
+	note D#, 1
+	note G#, 1
+	note D#, 1
+	note G#, 1
+	note D#, 1
+	note C_, 1
+	note G#, 1
+	note D#, 2
+	loopchannel 6, Music_RSLookYoungster_Ch4_loop_1
+	note C_, 2
+	note D#, 1
+	note D#, 1
+	note C_, 2
+	note D#, 1
+	note D#, 1
+	note C_, 2
+	note D#, 1
+	note D#, 1
+	note C_, 2
+	note D#, 1
+	note D#, 1
+	note C_, 1
+	note C_, 1
+	note D#, 1
+	note C_, 3
+	note D#, 1
+	note D#, 1
+	note B_, 4
+	loopchannel 0, Music_RSLookYoungster_Ch4_loop_main
